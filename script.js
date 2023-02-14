1e67a773f1ecb283456715cd087a27b79f0639e6
“vi3003” <vidhijain3003@gmail.com>
2023-02-14 21:09:42 +0530

    first commit

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
+const userGuess = document.getElementById('guess');
+const btn = document.getElementById('btn');
+const displayNum = document.getElementById('num');
+const hotOrCold = document.getElementById('respond');
+
+btn.addEventListener('click', ()=>{
+    let random = Math.floor((Math.random()*21));
+    let userNum = Number(userGuess.value);
+    displayNum.innerText = random;
+    if(random <= 20 && random >= -20){
+        hotOrCold.innerText = 'Hot';
+    }else{
+        hotOrCold.innerText = 'Cold';
+    }
+})
 
